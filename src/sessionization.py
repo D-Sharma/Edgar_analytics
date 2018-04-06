c837a0cdd21282b8a9c0d7506e1bf79c7f4691ad
Dimple Sharma <dimple.narang@gmail.com>
2018-04-06 10:19:06 -0700

    no message

--- src/sessionization.py
+++ src/sessionization.py
@@ -33,11 +33,17 @@ def process_stream(logfile, summaryfile, inactive_period):
         for line in stream:
             row = parse(line)
             exist = False
+            remove_i = None
             i = 0
             length = len(logs)
+            if length == 0:
+                logs.append(row)
+                continue
+
             for i in range(length):
                 log = logs[i]
                 if log["ipaddress"] == row["ipaddress"]:
+                    exist = True
                     # Increment count
                     document = log["cik_accession_extention"]
                     name, value = row["cik_accession_extention"].items()[0]
@@ -49,39 +55,41 @@ def process_stream(logfile, summaryfile, inactive_period):
                     # Session complete
                     start = log["access_datetime"]
                     end = row["access_datetime"]
-                    delta_in_sec = (start-end).total_seconds()
+                    delta_in_sec = (end-start).total_seconds()
                     print(delta_in_sec)
 
                     if(delta_in_sec == inactive_period):
-                        with open(summaryfile, "w+") as summary:
+                        with open(summaryfile, "a") as summary:
                             dict_length = len(document)
-                            new_row = ",".join([log["ipaddress"], start, end, delta_in_sec, dict_length])
+                            new_row = ",".join([log["ipaddress"], str(start), str(end), str(delta_in_sec), str(dict_length)])
+                            new_row = new_row + "\n"
                             summary.write(new_row)
-                        logs.remove(i)
+                        remove_i = i
                         break
 
-                else:
-                    logs.append(row)
+            if not exist:
+                logs.append(row)
+            elif remove_i:
+                logs.pop(remove_i)
+
     if len(logs) != 0:
+        exist = False
         i = 0
         log = logs[i]
         log_length = len(logs)
         for i in range(log_length):
-            # Increment count
-            document = log["cik_accession_extention"]
-            name, value = row["cik_accession_extention"].items()[0]
-            if name in document:
-                document[name] = document[name] + value
-            else:
-                document[name] = value
-            start = log["access_datetime"]
-            end = row["access_datetime"]
-            delta_in_sec = (start - end).total_seconds()
-            print(delta_in_sec)
-            with open(summaryfile, "w+") as summary:
-                dict_length = len(document)
-                new_row = ",".join([log["ipaddress"], start, end, delta_in_sec, dict_length])
-                summary.write(new_row)
+            if log["ipaddress"] == row["ipaddress"]:
+                log = logs[i]
+                document = log["cik_accession_extention"]
+                start = log["access_datetime"]
+                end = start
+                delta_in_sec = (end - start).total_seconds()
+                print(delta_in_sec)
+                with open(summaryfile, "a") as summary:
+                    dict_length = len(document)
+                    new_row = ",".join([log["ipaddress"], str(start), str(end), str(delta_in_sec), str(dict_length)])
+                    new_row = new_row + "\n"
+                    summary.write(new_row)
 
 
 def to_date(date_str, time_str):
